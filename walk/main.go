cc69c3af3941c47555f6cd578ce001be385f2ff6
Fausto Roger <hixian@gmail.com>
2024-12-20 03:05:46 -0300

    ✨ feat: Added new flag restore

--- walk/main.go
+++ walk/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"compress/gzip"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type config struct {
@@ -22,6 +24,8 @@ type config struct {
 	wLog io.Writer
 	// archive directory
 	archive string
+	// archive directory
+	restore string
 }
 
 func main() {
@@ -31,6 +35,7 @@ func main() {
 	// Action options
 	list := flag.Bool("list", false, "List files only")
 	archive := flag.String("archive", "", "Archive directory")
+	restore := flag.String("restore", "", "Restore directory")
 	del := flag.Bool("del", false, "Delete files")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
@@ -58,15 +63,18 @@ func main() {
 		del:     *del,
 		wLog:    f,
 		archive: *archive,
+		restore: *restore,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
 }
 
 func run(root string, out io.Writer, cfg config) error {
+
 	delLogger := log.New(cfg.wLog, "DELETED FILE: ", log.LstdFlags)
 
 	return filepath.Walk(root,
@@ -91,6 +99,13 @@ func run(root string, out io.Writer, cfg config) error {
 				}
 			}
 
+			// Restore files and continue if successful
+			if cfg.restore != "" {
+				if err := restoreFile(cfg.restore, root, path); err != nil {
+					return err
+				}
+			}
+
 			// Delete files
 			if cfg.del {
 				return delFile(path, delLogger)
@@ -99,4 +114,55 @@ func run(root string, out io.Writer, cfg config) error {
 			// List is the default option if nothing else was set
 			return listFile(path, out)
 		})
+}
+
+// restoreFile function to restore the root path into the destination directory
+func restoreFile(destDir, root, path string) error {
+	info, err := os.Stat(destDir)
+	if err != nil {
+		return err
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("%s is not a directory", destDir)
+	}
+
+	relDir, err := filepath.Rel(root, filepath.Dir(path))
+	if err != nil {
+		return err
+	}
+
+	dest := strings.TrimSuffix(filepath.Base(path), ".gz")
+	targetPath := filepath.Join(destDir, relDir, dest)
+
+	if err := os.MkdirAll(filepath.Dir(targetPath), 0755); err != nil {
+		return err
+	}
+
+	out, err := os.OpenFile(targetPath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	in, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	zr, err := gzip.NewReader(in)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(out, zr); err != nil {
+		return err
+	}
+
+	if err := zr.Close(); err != nil {
+		return err
+	}
+
+	return out.Close()
 }
